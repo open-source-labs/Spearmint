af2ad0e3cb86be8d48164e6e8db7cc82c35e9018
Alex Park <apark0720@gmail.com>
2019-05-20 21:04:03 -0700

    first commit

---
+++ electron/main.js
@@ -0,0 +1,27 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const isDev = require("electron-is-dev");
+let mainWindow;
+
+function createWindow() {
+mainWindow = new BrowserWindow({ width: 900, height: 680 });
+mainWindow.loadURL(
+isDev
+? "http://localhost:3000"
+: `file://${path.join(__dirname, "../build/index.html")}`
+);
+mainWindow.on("closed", () => (mainWindow = null));
+}
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+if (process.platform !== "darwin") {
+app.quit();
+}
+});
+app.on("activate", () => {
+if (mainWindow === null) {
+createWindow();
+}
+});
