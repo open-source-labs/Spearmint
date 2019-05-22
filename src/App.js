70f401c14d9c08ade06c8bf264fae7656429cb00
julicious100 <beakjulie@gmail.com>
2019-05-22 14:40:39 -0700

    before merge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+
 import NavBar from "./containers/NavBar";
 import LeftPanel from "./containers/LeftPanel";
 
+const styles = {
+  fontFamily: "sans="
+};
 const App = () => (
   <>
     <NavBar />
