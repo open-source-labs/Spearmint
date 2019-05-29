79f415c2a2ac7e3b0bf27573e41aa5008734208c
julicious100 <beakjulie@gmail.com>
2019-05-28 20:35:41 -0700

    started working on styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { useState, useReducer, createContext } from 'react';
-import NavBar from './containers/NavBar';
-import LeftPanel from './containers/LeftPanel';
-import ProjectLoader from './containers/ProjectLoader';
-import RightPanel from './containers/RightPanel';
-import { TestCaseContext, testCaseState, testCaseReducer } from "./context/testCaseReducer";
-import { MockDataContext, mockDataState, mockDataReducer } from "./context/mockDataReducer";
+import React, { useState, useReducer, createContext } from "react";
+import withStyles from "react-jss";
+import NavBar from "./containers/NavBar";
+import LeftPanel from "./containers/LeftPanel";
+import ProjectLoader from "./containers/ProjectLoader";
+import RightPanel from "./containers/RightPanel";
+import {
+  TestCaseContext,
+  testCaseState,
+  testCaseReducer
+} from "./context/testCaseReducer";
+import {
+  MockDataContext,
+  mockDataState,
+  mockDataReducer
+} from "./context/mockDataReducer";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlus,
   faMinus,
@@ -22,68 +30,74 @@ export const LoadedContext = createContext(null);
 library.add(faPlus, faMinus, faTimes, faQuestionCircle);
 
 const styles = {
+  app: {
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 2rem)"
+  }
+};
+const styles1 = {
   fontFamily: "arial",
-  display: "flex",
+  display: "flex"
 };
 
 const loaderDiv = {
-  display:"flex",
+  display: "flex",
   justifyContent: "center"
-}
+};
 
 const App = () => {
-  const [fileCode, setFileCode] = useState('');
+  const [fileCode, setFileCode] = useState("");
   const [fileTree, setFileTree] = useState(null);
   const [loaded, setLoaded] = useState(false);
   const [testCase, dispatchTestCase] = useReducer(
     testCaseReducer,
     testCaseState
   );
-    console.log(testCase);
+  console.log(testCase);
   const [mockData, dispatchMockData] = useReducer(
     mockDataReducer,
     mockDataState
   );
 
-    if(!loaded) {
+  if (!loaded) {
     return (
       <div style={loaderDiv}>
-      <>
-        <FileTreeContext.Provider value={setFileTree}>
-          <LoadedContext.Provider value={setLoaded}>
-            <ProjectLoader />
-          </LoadedContext.Provider>
-        </FileTreeContext.Provider>
-      </>
-      </div>
-    )} 
-    else {
-      return (
-    <div style={styles}>
-      <>
-        <FileCodeContext.Provider value={setFileCode}>
-          <FileTreeContext.Provider value={fileTree}>
-            <TestCaseContext.Provider value={testCase}>
-              <MockDataContext.Provider value={mockData}>
-                <NavBar />
-              </MockDataContext.Provider>
-            </TestCaseContext.Provider>
+        <>
+          <FileTreeContext.Provider value={setFileTree}>
+            <LoadedContext.Provider value={setLoaded}>
+              <ProjectLoader />
+            </LoadedContext.Provider>
           </FileTreeContext.Provider>
-        </FileCodeContext.Provider>
+        </>
+      </div>
+    );
+  } else {
+    return (
+      <div style={styles}>
+        <div style={styles1}>
+          <FileCodeContext.Provider value={setFileCode}>
+            <FileTreeContext.Provider value={fileTree}>
+              <TestCaseContext.Provider value={testCase}>
+                <MockDataContext.Provider value={mockData}>
+                  <NavBar />
+                </MockDataContext.Provider>
+              </TestCaseContext.Provider>
+            </FileTreeContext.Provider>
+          </FileCodeContext.Provider>
 
-        <TestCaseContext.Provider value={[testCase, dispatchTestCase]}>
-          <MockDataContext.Provider value={[mockData, dispatchMockData]}>
-            <LeftPanel />   
-          </MockDataContext.Provider>
-        </TestCaseContext.Provider>
-        
-        <FileCodeContext.Provider value={fileCode}>
-          <RightPanel />        
-        </FileCodeContext.Provider>
-      </>
-    </div>
-    )
+          <TestCaseContext.Provider value={[testCase, dispatchTestCase]}>
+            <MockDataContext.Provider value={[mockData, dispatchMockData]}>
+              <LeftPanel />
+            </MockDataContext.Provider>
+          </TestCaseContext.Provider>
+
+          <FileCodeContext.Provider value={fileCode}>
+            <RightPanel />
+          </FileCodeContext.Provider>
+        </div>
+      </div>
+    );
   }
-}
+};
 
 export default App;
