d2436b6f53f0cd6c11124a744b5b8eac69e0719f
julicious100 <beakjulie@gmail.com>
2019-06-01 11:32:33 -0700

    auto complete works!

--- src/components/LeftPanel/TestFile/Action.jsx
+++ src/components/LeftPanel/TestFile/Action.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import { deleteAction, updateAction } from '../../../context/testCaseActions';
+import Autosuggest from 'react-autosuggest';
+import { events } from './ActionEvents';
 const minusIcon = require('../../../assets/images/minus3.svg');
 const questionIcon = require('../../../assets/images/question.svg');
+
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0
+    ? []
+    : events.filter(e => e.name.toLowerCase().slice(0, inputLength) === inputValue);
+};
+
+const getSuggestionValue = suggestion => suggestion.name;
+const renderSuggestion = suggestion => <div>{suggestion.name}</div>;
 
 const Action = ({ id, dispatchTestCase }) => {
   const [eventType, setEventType] = useState('');
@@ -9,6 +23,8 @@ const Action = ({ id, dispatchTestCase }) => {
   const [queryVariant, setQueryVariant] = useState('');
   const [querySelector, setQuerySelector] = useState('');
   const [queryValue, setQueryValue] = useState('');
+  const [eventName, setEventName] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
   const handleClickDelete = e => {
     dispatchTestCase(deleteAction(id));
@@ -62,6 +78,79 @@ const Action = ({ id, dispatchTestCase }) => {
     return eventsWithValues.includes(eventType);
   };
 
+  const onChange = (event, { newValue }) => {
+    setEventName(newValue);
+  };
+
+  const onSuggestionsFetchRequested = ({ value }) => {
+    setSuggestions(getSuggestions(value));
+  };
+
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const inputProps = {
+    placeholder: 'Enter an action',
+    value: eventName,
+    onChange,
+  };
+
+  const theme = {
+    container: {
+      position: 'relative',
+    },
+    input: {
+      width: 120,
+      height: 0,
+      padding: '10px 20px',
+      fontFamily: 'Helvetica, sans-serif',
+      fontWeight: 300,
+      fontSize: 16,
+      border: '1px solid #aaa',
+      borderTopLeftRadius: 4,
+      borderTopRightRadius: 4,
+      borderBottomLeftRadius: 4,
+      borderBottomRightRadius: 4,
+    },
+    inputFocused: {
+      outline: 'none',
+    },
+    inputOpen: {
+      borderBottomLeftRadius: 0,
+      borderBottomRightRadius: 0,
+    },
+    suggestionsContainer: {
+      display: 'none',
+    },
+    suggestionsContainerOpen: {
+      display: 'block',
+      position: 'absolute',
+      top: 51,
+      width: 280,
+      border: '1px solid #aaa',
+      backgroundColor: '#fff',
+      fontFamily: 'Helvetica, sans-serif',
+      fontWeight: 300,
+      fontSize: 16,
+      borderBottomLeftRadius: 4,
+      borderBottomRightRadius: 4,
+      zIndex: 2,
+    },
+    suggestionsList: {
+      margin: 0,
+      padding: 0,
+      listStyleType: 'none',
+    },
+    suggestion: {
+      cursor: 'pointer',
+      padding: '10px 20px',
+    },
+    suggestionHighlighted: {
+      backgroundColor: '#ddd',
+    },
+  };
+
   return (
     <div>
       <h3>Action</h3>
@@ -80,13 +169,22 @@ const Action = ({ id, dispatchTestCase }) => {
         />
       </svg>
       <label htmlFor='event-type'>Event Type</label>
-      <input type='text' id='event-type' onChange={handleChangeEventType} />
+      <Autosuggest
+        theme={theme}
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      />
+      {/* <input type='text' id='event-type' onChange={handleChangeEventType} />
       {needsEventValue(eventType) && (
         <span>
           <label htmlFor='event-value' />
           <input type='text' id='event-type' onChange={handleChangeEventValue} />
         </span>
-      )}
+      )} */}
 
       <label htmlFor='queryVariant'>Query Selector</label>
       <svg
