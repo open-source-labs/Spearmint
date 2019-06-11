e6782ee4f63135b90699bce62c88c3f53948e014
Alex Park <apark0720@gmail.com>
2019-06-11 16:32:38 -0700

    add test suite

---
+++ src/containers/ProjectLoader/__tests__/projectLoader.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import ProjectLoader from '../ProjectLoader';
+import { render, fireEvent } from '@testing-library/react';
+import { build, fake } from 'test-data-bot';
+import '@testing-library/react/cleanup-after-each';
+import 'jest-dom/extend-expect'
+
+test('displays browser view of when URL is correctly inputted', () => {
+});
+
+test(`ommitting 'www' from URL still displays browser view`, () => {
+});
+
+test('localhost URL displays on the browser view', () => {
+});
+
+test('displays file directory of project that is loaded', () => {
+});
+
+test(`displays project's name on the top of the file directory view`, () => {
+});
+
+
+
+
+
