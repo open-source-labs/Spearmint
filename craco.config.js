515cdd5077253205bfc8e8a4c121b193986bf4f4
julicious100 <beakjulie@gmail.com>
2019-06-12 12:51:49 -0700

    spectron testing is about to start

--- craco.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  webpack: {
-    configure: {
-      target: 'electron-renderer',
-    },
-  },
-};
---
+++ src/__tests__/spec.test.js
@@ -0,0 +1,28 @@
+const Application = require('spectron').Application;
+const assert = require('assert');
+const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
+const path = require('path');
+
+describe('Application launch', function() {
+  beforeEach(() => {
+    this.app = new Application({
+      path: electronPath,
+      args: [path.join(__dirname, '../../electron/main.js')],
+    });
+    return this.app.start();
+  });
+
+  afterEach(() => {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
+  });
+
+  test('shows an initial window', async () => {
+    return this.app.client.getWindowCount().then(function(count) {
+      assert.equal(count, 1);
+      // Please note that getWindowCount() will return 2 if `dev tools` are opened.
+      // assert.equal(count, 2)
+    });
+  });
+});
