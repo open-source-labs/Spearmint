a7eee1ab9aecd54f87339e63f0de7e7f9318f25c
Charlie Maloney <charliemaloney200@gmail.com>
2020-05-23 15:57:59 -0700

    fixed electron build process

--- public/electron.js
+++ public/electron.js
@@ -1,16 +1,19 @@
+require('dotenv').config()
 const { app, BrowserWindow } = require('electron');
-const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 const path = require('path');
 const isDev = require('electron-is-dev');
 let mainWindow;
 
-function addDevTools() {
-  app.whenReady().then(() => {
-  installExtension(REACT_DEVELOPER_TOOLS)
-      .then((name) => console.log(`Added Extension:  ${name}`))
-      .catch((err) => console.log('An error occurred: ', err));
-});
-};
+if (process.env.NODE_ENV === 'development') {
+  const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
+  function addDevTools() {
+    app.whenReady().then(() => {
+    installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
+  });
+  };
+}
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1550,
@@ -27,7 +30,11 @@ function createWindow() {
   );
   mainWindow.on('closed', () => (mainWindow = null));
 }
-app.on('ready', addDevTools);
+
+if (process.env.NODE_ENV === 'development') {
+  app.on('ready', addDevTools);
+}
+
 app.on('ready', createWindow);
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
