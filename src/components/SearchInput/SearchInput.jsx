0f77ac183128f3a0172e31ca09c9d9ab574f2344
Annie S <annieshin1997@gmail.com>
2021-03-13 20:46:19 -0800

    clean eslint rules

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState } from 'react';
 import './SearchInput.scss';
 
