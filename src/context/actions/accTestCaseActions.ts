381fde21b9db4d62112b8b4e67840c9e4ab6ef81
Tolan Thornton <taoantaoan@hotmail.com>
2021-03-23 22:59:23 -0700

    Add types to testType and puppeteerUrl

--- src/context/actions/accTestCaseActions.ts
+++ src/context/actions/accTestCaseActions.ts
@@ -85,12 +85,12 @@ export const updateFilePath = (fileName: string, filePath: string) => ({
   filePath,
 });
 
-export const updateTestType = (testType) => ({
+export const updateTestType = (testType: string) => ({
   type: actionTypes.UPDATE_TEST_TYPE,
   testType,
 });
 
-export const createPuppeteerUrl = (puppeteerUrl) => ({
+export const createPuppeteerUrl = (puppeteerUrl: string) => ({
   type: actionTypes.CREATE_PUPPETEER_URL,
   puppeteerUrl,
 });
