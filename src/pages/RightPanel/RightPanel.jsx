1c0735540aa397bc9c8903045489797f50b572ac
“mhmaidi789” <mo@Mahmouds-MBP.local>
2021-04-20 19:37:06 -0500

    Add checkbox to disable mouse clicks in BrowserView Component
    Co-authored-by: Justin Baik <bij3377@gmail.com>
    Co-authored-by: Max Weisenberger <germanbluemax@gmail.com>
    Co-authored-by: Dieu Huynh <dieu.h.huynh@gmail.com>

--- src/pages/RightPanel/RightPanel.jsx
+++ src/pages/RightPanel/RightPanel.jsx
@@ -36,7 +36,7 @@ const RightPanel = () => {
       <div hidden={tabIndex !== 0}>
         <EditorView />
       </div>
-      {/* Browser Tab */}
+      {/* rBrowser Tab */}
       <div hidden={tabIndex !== 1}>
         <BrowserView />
       </div>
@@ -45,7 +45,6 @@ const RightPanel = () => {
         <div className="terminal">
           <TerminalGenerator />
         </div>
-        
       </div>
       {/* Test Coverage */}
       <div hidden={tabIndex !== 3}>
