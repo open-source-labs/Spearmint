f6766ccae57f5cdc74e153228a58f6ffa4f3b651
Dieu Huynh <dieuhuynh@Dieus-MacBook-Pro.local>
2021-04-22 20:12:32 -0700

    Added accessibility lens: muted
    Co-authored-by: Justin Baik <bij3377@gmail.com>
    Co-authored-by: Max Weisenberger <germanbluemax@gmail.com>
    Co-authored-by: Mo Hmaidi <mhmaidi789@gmail.com>

--- src/pages/RightPanel/RightPanel.jsx
+++ src/pages/RightPanel/RightPanel.jsx
@@ -37,7 +37,7 @@ const RightPanel = () => {
       <div hidden={tabIndex !== 0}>
         <EditorView />
       </div>
-      {/* rBrowser Tab */}
+      {/* Browser Tab */}
       <div hidden={tabIndex !== 1}>
         <BrowserView />
       </div>
