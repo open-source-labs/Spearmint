dc0b482cef50a7efef12a9edcc65e1d0971a90ca
Max Weisenberger <germanbluemax@gmail.com>
2021-04-28 19:03:29 -0700

    Removed main process sending shell to render process.
    Co-authored-by: Justin Baik <bij3377@gmail.com>
    Co-authored-by: Max Weisenberger <germanbluemax@gmail.com>
    Co-authored-by: Mo Hmaidi <mhmaidi789@gmail.com>
    Co-authored-by: Dieu Huynh <dieuhhuynh@gmail.com>

--- public/electron.js
+++ public/electron.js
@@ -45,9 +45,6 @@ function createWindow() {
   );
   mainWindow.on('closed', () => (mainWindow = null));
 
- // Send shell type to modal for custom terminal commands
-  mainWindow.webContents.send('Modal.shellType', shell)
-
   // PTY PROCESS FOR IN APP TERMINAL
   const ptyProcess = pty.spawn(shell, [], {
     name: 'xterm-color',
