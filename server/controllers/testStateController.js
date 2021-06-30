56b796db0c8f5c0791bfe8c847342b4e616347a6
Eric Park <ericparkmusic@gmail.com>
2021-06-29 23:20:53 -0700

    Changed route logic for /getTests and added comments

--- server/controllers/testStateController.js
+++ server/controllers/testStateController.js
@@ -1,7 +1,8 @@
+// Import test state model that defines the structure of test stored in DB
 const TestState = require('../models/testStateModel');
-
 const testStateController = {};
 
+// Middleware to upload a passed test into DB
 testStateController.upload = (req, res, next) => {
   TestState.create(
     {
@@ -17,10 +18,12 @@ testStateController.upload = (req, res, next) => {
   );
 };
 
+// Middleware too get all saved tests of current user and of selected type
 testStateController.getTests = (req, res, next) => {
   TestState.find({ userId: req.cookies.ssid, testType: req.params.testType }, (err, result) => {
+    // If an error occurs, invoke error handler with err object
     if (err) return next(err);
-    if (result.length === 0) return next('User Has No Saved Tests');
+    // Save resulting tests array to locals object
     res.locals.tests = result;
     return next();
   });
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,38 +1,73 @@
-const path = require('path');
+// Import Express to streamline server logic with router
 const express = require('express');
-
+// Import all relevant controller objects equipped with middleware
 const userController = require('../controllers/userController');
 const cookieController = require('../controllers/cookieController');
 const sessionController = require('../controllers/sessionController');
 const testStateController = require('../controllers/testStateController');
+// Initialize an express router
 const router = express.Router();
 
-router.post('/signup', userController.bcrypt, userController.signup, (req, res) => {
-  res.status(200).json('Sign Up Successful');
-});
+// Set up route for post requests to /signup
+router.post(
+  '/signup',
+  // Bcrypt middleware to encrypt user password
+  userController.bcrypt,
+  // Signup middleware to sign user up with encrypted credentials
+  userController.signup,
+  // Anonymous middleware to send back valid response
+  (req, res) => {
+    res.status(200).json('Sign Up Successful');
+  }
+);
 
+// Set up route for post requests to /login
 router.post(
   '/login',
+  // Login middleware checks encrypted credentials
   userController.login,
+  // Cookie middleware to set up a new cookie
   cookieController.setSSIDCookie,
+  // Session middleware to initialize new session
   sessionController.startSession,
+  // Anonymous middleware to send back valid response
   (req, res) => {
     res.status(200).json({ ssid: res.locals.ssid });
   }
 );
 
-router.get('/logout', sessionController.endSession, (req, res) => {
-  res.status(200).json('Logged Out Successfully');
-});
+// Set up route for get requests to /logout
+router.get(
+  '/logout',
+  // Session middleware to end any existing sessions
+  sessionController.endSession,
+  // Anonymous middleware to send back valid response
+  (req, res) => {
+    res.status(200).json('Logged Out Successfully');
+  }
+);
 
-router.post('/upload', sessionController.isLoggedIn, testStateController.upload, (req, res) => {
-  res.status(200).json('Test Uploaded Successfully');
-});
+// Set up route for post requests to /upload
+router.post(
+  '/upload',
+  // Session middleware to check if current user is signed in
+  sessionController.isLoggedIn,
+  // Upload middleware to save passed test object into DB
+  testStateController.upload,
+  // Anonymous middleware to send back valid response
+  (req, res) => {
+    res.status(200).json('Test Uploaded Successfully');
+  }
+);
 
+// Set up route for get requests to /getTests with type passed as param
 router.get(
   '/getTests/:testType',
+  // Session middleware to check if current user is signed in
   sessionController.isLoggedIn,
+  // GetTests middleware to retrieve all saved tests from DB
   testStateController.getTests,
+  // Anonymous middleware to send back valid response
   (req, res) => {
     res.status(200).json(res.locals.tests);
   }
