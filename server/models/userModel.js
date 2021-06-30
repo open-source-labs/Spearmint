a3550d19b6d7293eebb2291f29d9f9afd488b899
Eric Park <ericparkmusic@gmail.com>
2021-06-30 15:09:37 -0700

    Saved MongoDB password to .env file

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,8 +1,8 @@
+require('dotenv').config({ path: __dirname + '/./../../.env' });
 // Import mongoose for MongoDB object modeling
 const mongoose = require('mongoose');
 
-const MONGO_URI =
-  'mongodb://ericgpark:jENk%40eW6QEGez-g@spearmint-shard-00-00.xbncu.mongodb.net:27017,spearmint-shard-00-01.xbncu.mongodb.net:27017,spearmint-shard-00-02.xbncu.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-rbxspt-shard-0&authSource=admin&retryWrites=true&w=majority';
+const MONGO_URI = `mongodb://ericgpark:${process.env.MONGO_URI}@spearmint-shard-00-00.xbncu.mongodb.net:27017,spearmint-shard-00-01.xbncu.mongodb.net:27017,spearmint-shard-00-02.xbncu.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-rbxspt-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
