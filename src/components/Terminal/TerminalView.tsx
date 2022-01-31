6111b53687900f09980de3fb0bf337cce2750b78
Ruzeb <rc647@cornell.edu>
2022-01-31 02:22:00 -0500

    todo fix the terminal

--- src/components/Terminal/TerminalView.tsx
+++ src/components/Terminal/TerminalView.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, ChangeEvent } from 'react';
 import { XTerm } from 'xterm-for-react';
 import { TerminalType } from '../../utils/terminalTypes';
+import { FitAddon } from 'xterm-addon-fit';
 
 const { Terminal } = require('xterm');
 const ipc = require('electron').ipcRenderer;
 
-const terminalArgs: TerminalType = {
+const terminalArgs = {
   fontSize: 15,
   // Currently rows are hardcoded, next step is to make terminal sizing dynamic.
-  rows: 30,
   fontFamily: 'monospace',
   theme: {
     background: '#002a36',
@@ -16,11 +16,13 @@ const terminalArgs: TerminalType = {
 };
 
 const term = new Terminal(terminalArgs);
+const fitAddon = new FitAddon();
 
 const TerminalView = () => {
   useEffect(() => {
-    // console.log(global.projectFilePath);
+    term.loadAddon(fitAddon);
     term.open(document.getElementsByClassName('terminal')[0]);
+    fitAddon.fit();
     // when we have input events (e), we would send the data to the main processor
     term.onData((e: ChangeEvent) => {
       ipc.send('terminal.toTerm', e);
@@ -32,7 +34,10 @@ const TerminalView = () => {
     });
   }, []);
 
-  return <XTerm className="terminal" />;
+  return (
+  <div className="terminal" style={{ width: '500px', height:"100%" }}/>
+
+  )
 };
 
 export default TerminalView;
