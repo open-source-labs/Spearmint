2adef37529a58137c3b48005a1d2aea4b441beeb
WT <wilsonwttran@gmail.com>
2022-10-02 22:46:39 -0700

    sec, solid, svelte, vue reset test feature added

--- src/components/TestMenu/SolidTestMenu.jsx
+++ src/components/TestMenu/SolidTestMenu.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import styles from '../TestMenu/TestMenu.module.scss';
 import { GlobalContext } from '../../context/reducers/globalReducer';
 import { openBrowserDocs } from '../../context/actions/globalActions';
-import { addDescribeBlock, createNewTest } from '../../context/actions/frontendFrameworkTestCaseActions';
+import { addDescribeBlock, createNewTest, resetTests } from '../../context/actions/frontendFrameworkTestCaseActions';
 import Modal from '../Modals/Modal';
 import useGenerateTest from '../../context/useGenerateTest.jsx';
 import { MockDataContext } from '../../context/reducers/mockDataReducer';
@@ -20,6 +20,7 @@ import { SolidTestCaseContext } from '../../context/reducers/solidTestCaseReduce
 import TestMenuButtons from './TestMenuButtons';
 import { useToggleModal, validateInputs } from './testMenuHooks';
 import ExportFileModal from '../Modals/ExportFileModal';
+import { clearMockData } from '../../context/actions/mockDataActions';
 const { ipcRenderer } = require('electron');
 
 // imports were declared in previous iterations, but were never used
@@ -87,11 +88,17 @@ const SolidTestMenu = () => {
     if (!isTestModalOpen) dispatchToGlobal(toggleModal());
   };
 
+  const handleResetTests = () => {
+    dispatchToSolidTestCase(resetTests());
+    dispatchToMockData(clearMockData());
+  }
+
   if (!file && exportBool) dispatchToGlobal(updateFile(generateTest(SolidTestCase, mockData)));
 
   return (
     <>
       <TestMenuButtons 
+        resetTests={handleResetTests}
         openModal={openModal}
         fileHandle={fileHandle}
         openScriptModal={openScriptModal}
