3cba616ce89c6bd74099f0ec7e06be8c5d35cb1d
Cartlong7 <crlong7@gmail.com>
2023-05-08 11:09:44 -0600

    added test for displaying test name on page

--- src/__tests__/reduxTestCase.test.js
+++ src/__tests__/reduxTestCase.test.js
@@ -7,12 +7,26 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ReduxTestCase from '../components/TestCase/ReduxTestCase';
 import { act } from 'react-dom/test-utils'
+import ReduxTestMenu from '../components/TestMenu/ReduxTestMenu';
 
-xdescribe('should render ReduxTestCase component', () => {
+describe('should render ReduxTestCase component', () => {
   // const { getByRole, debug } = act(() => { render(<ReduxTestCase />) });
-
+  
   it('displays the component', () => {
     act(() => { render(<ReduxTestCase />) });
     screen.debug()
   })
+
+  it('displays the name of the test component to be at the top of the page', () => {
+    render(<ReduxTestCase/>);
+    const testName = screen.getByText(/redux testing/i);
+    expect(testName).toBeInTheDocument();
+  })
 });
+
+describe('should render the ReduxTestMenu component', () => {
+  render(<ReduxTestMenu/>)
+  it('displays the test menu component', () => {
+    
+  })
+})
