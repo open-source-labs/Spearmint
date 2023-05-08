8e01fe69afba05c4fca2037c98fea3514abab944
Cartlong7 <crlong7@gmail.com>
2023-05-08 14:51:57 -0600

    added testing for ReduxTestMenu and TestMenuButton components

--- src/__tests__/reduxTestCase.test.js
+++ src/__tests__/reduxTestCase.test.js
@@ -8,25 +8,36 @@ import '@testing-library/jest-dom/extend-expect';
 import ReduxTestCase from '../components/TestCase/ReduxTestCase';
 import { act } from 'react-dom/test-utils'
 import ReduxTestMenu from '../components/TestMenu/ReduxTestMenu';
+import TestMenuButtons from '../components/TestMenu/TestMenuButtons';
 
 describe('should render ReduxTestCase component', () => {
   // const { getByRole, debug } = act(() => { render(<ReduxTestCase />) });
-  
+  beforeEach(() => {
+    render(<ReduxTestCase/>);
+  });
+
   it('displays the component', () => {
-    act(() => { render(<ReduxTestCase />) });
     screen.debug()
   })
 
   it('displays the name of the test component to be at the top of the page', () => {
-    render(<ReduxTestCase/>);
     const testName = screen.getByText(/redux testing/i);
     expect(testName).toBeInTheDocument();
   })
 });
 
 describe('should render the ReduxTestMenu component', () => {
-  render(<ReduxTestMenu/>)
-  it('displays the test menu component', () => {
-    
+  beforeEach(() => {
+    render(<TestMenuButtons/>)
   })
+
+  it('displays the test menu component', () => {
+    screen.debug();
+  });
+
+  it('displays all five test menu buttons', () => {
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(5);
+    expect(buttons).not.toBeNull();
+  });
 })
