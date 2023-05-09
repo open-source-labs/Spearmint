0cc8f9abe95c9acb525aa386eaa81799bcd7af94
Cartlong7 <crlong7@gmail.com>
2023-05-08 19:20:07 -0600

    updated tests for ReduxTestCase describe block and others

--- src/__tests__/reduxTestCase.test.js
+++ src/__tests__/reduxTestCase.test.js
@@ -3,13 +3,14 @@
  */
 
 import React from 'react';
-import { render, screen, within } from '@testing-library/react';
+import { render, screen, within, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ReduxTestCase from '../components/TestCase/ReduxTestCase';
 import { act } from 'react-dom/test-utils'
 import ReduxTestMenu from '../components/TestMenu/ReduxTestMenu';
 import TestMenuButtons from '../components/TestMenu/TestMenuButtons';
 import { ReduxTestCaseState } from '../utils/reduxTypes';
+import { IconButton } from '@mui/material';
 
 describe('should render ReduxTestCase component', () => {
   beforeEach(() => {
@@ -24,6 +25,11 @@ describe('should render ReduxTestCase component', () => {
     const testName = screen.getByText(/redux testing/i);
     expect(testName).toBeInTheDocument();
   })
+
+  it('displays the input describe field.', () => {
+    const input = screen.getByLabelText(/describe block/i);
+    expect(input).toBeInTheDocument();
+  })
 });
 
 describe('should render the ReduxTestMenu component.', () => {
@@ -37,7 +43,8 @@ describe('should render the ReduxTestMenu component.', () => {
   })
 
   it('handles resetting the tests', () => {
-    
+    const mock = jest.fn();
+
   })
 })
 
@@ -62,4 +69,14 @@ describe('should render the TestMenuButtons component', () => {
     })
   });
 
+  //needs work
+  xit('opens the redux testing docs on click', () => {
+    const openDocs = jest.fn();
+    render(<TestMenuButtons onClick={openDocs}/>)
+
+    const helpButton = screen.getByTitle('Need Help?');
+
+    fireEvent.click(helpButton);
+    expect(openDocs).toHaveBeenCalled();
+  })
 })
