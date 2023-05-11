520a4bd25c42eab0dadb579034acff69d67d75cf
Cartlong7 <crlong7@gmail.com>
2023-05-11 14:03:13 -0600

    install of userEvent dependency, removed unused dependencies, added userEvent test

--- src/__tests__/reduxTestCase.test.js
+++ src/__tests__/reduxTestCase.test.js
@@ -6,12 +6,9 @@ import React from 'react';
 import { render, screen, within, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ReduxTestCase from '../components/TestCase/ReduxTestCase';
-import { act } from 'react-dom/test-utils'
 import ReduxTestMenu from '../components/TestMenu/ReduxTestMenu';
 import TestMenuButtons from '../components/TestMenu/TestMenuButtons';
-import { ReduxTestCaseState } from '../utils/reduxTypes';
-import { IconButton } from '@mui/material';
-import { ReduxTestCaseContext } from '../context/reducers/reduxTestCaseReducer';
+import userEvent from '@testing-library/user-event'
 
 const dispatchToReduxTestCase = jest.fn();
 const dispatchToGlobal = jest.fn();
@@ -43,16 +40,16 @@ describe('should render ReduxTestCase component', () => {
 
 describe('should render the ReduxTestMenu component.', () => {
 
-  it('opens the Redux testing docs on click', () => {
+  it('should pass openDocs function as prop to TestMenuButtons', () => {
     const openDocs = jest.fn();
 
     render(<ReduxTestMenu openDocs={openDocs} dispatchToGlobal={dispatchToGlobal}/>);
-
+    expect(TestMenuButtons).toHaveBeenCalled();
     // Assert that the openDocs function was called
-    expect(screen.getByText(/openDocs/i).toBeInTheDocument());
+    // expect(screen.getByText(/openDocs/i).toBeInTheDocument());
   });
 
-  xit('should pass openDocs function as prop to TestMenuButtons', async () => {
+  xit('checks that openDocs prop is a function on TestMenuButtons', async () => {
     const { getByDisplayName } = render(<ReduxTestMenu />);
     const TestMenuButtonsComponent = await getByDisplayName('TestMenuButtons');
     const openDocsProp = TestMenuButtonsComponent.props.openDocs;
@@ -81,14 +78,12 @@ describe('should render the TestMenuButtons component', () => {
     })
   });
 
-  it('opens the redux testing docs on click', () => {
-    const openDocs = jest.fn();
-
-    render(<TestMenuButtons openDocs={openDocs}/>)
-
-    const helpButton = screen.getByTitle('Need Help?');
-
-    fireEvent.click(helpButton);
-    expect(openDocs).toHaveBeenCalled();
+  it('opens the redux testing docs on click', async () => {
+    const props = {
+      openDocs: jest.fn()
+    }
+    render(<TestMenuButtons {...props}/>)
+    await userEvent.click(screen.getByTitle('Need Help?'));
+    expect(props.openDocs).toHaveBeenCalledTimes(1);
   })
 })
