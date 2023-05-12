5f5cad1f1fc47a89f49b56e7c8043a90abe672a7
Cartlong7 <crlong7@gmail.com>
2023-05-12 15:30:29 -0600

    removed extraneous imports from redux tests. added new test file for ActionCreator

---
+++ src/__tests__/reduxActionCreator.test.js
@@ -0,0 +1,9 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom/extend-expect';
+import ActionCreator from '../components/ReduxTestComponent/ActionCreator/ActionCreator'
