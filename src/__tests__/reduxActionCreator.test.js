a60f1e92d699b940cff72fb4e334b3f36bbf74a2
Cartlong7 <crlong7@gmail.com>
2023-05-13 15:03:04 -0600

    updated test suite to reflect best practices. All tests now passing

--- src/__tests__/reduxActionCreator.test.js
+++ src/__tests__/reduxActionCreator.test.js
@@ -7,7 +7,20 @@ import { fireEvent, render, screen,  cleanup, getByPlaceholderText, waitFor } fr
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect';
 import ActionCreator from '../components/ReduxTestComponent/ActionCreator/ActionCreator'
-import { ReduxTestCaseContext } from '../context/reducers/reduxTestCaseReducer';
+
+/* 
+A note on testing in React Testing Library: 
+  In order for user interaction to work and to persist through the state of your test you need to remember four things:
+    1) The state defined in your component when it is rendered in the test MUST match the type and value found on every property (see below)
+    2) Often the property types defined in the reducer may not reflect the actual values and types when the application is compiled from TS and rendered
+      i.e. The properties below of actionType and payloadKey must be null as the value associated with them before any text is entered in their input field is null
+    3) userEvent is best practice over fireEvent, and must always be used asynchronously (await userEvent)
+    4) Do not attempt to test the functionality of the components, rather test the user interaction and what is displayed in the DOM
+  
+  More reading: https://kentcdodds.com/blog/common-mistakes-with-react-testing-library
+
+-Spearmint V.14
+*/
 
 const props = {
   actionCreator: {
@@ -17,23 +30,16 @@ const props = {
     typesFileName: '',
     typesFilePath: '',
     type: 'action-creator',
-    actionCreatorFunc: '',
+    actionCreatorFunc: null,
     actionType: null,
     payloadKey: null,
     payloadType: null,
-    it: '', 
+    it: null, 
     },
   index: 0
 }
 
-const state = {
-  reduxTestStatements: '',
-  reduxStatments: [props]
-};
-
-xdescribe('Action Creator', () => {
-  
-  afterEach(cleanup);
+describe('Action Creator', () => {
 
   it('displays the ActionCreator component', () => {
     render(<ActionCreator {...props}/>);
@@ -56,9 +62,9 @@ xdescribe('Action Creator', () => {
   })
 
   it('updates the It should... input field value on user input', async () => {
-    const { getByLabelText } = render(<ActionCreator {...props}/>);
+    render(<ActionCreator {...props}/>);
 
-    const it = getByLabelText('It should...');
+    const it = screen.getByLabelText('It should...');
     expect(it).toBeInTheDocument();
     expect(it.placeholder).toBe('e.g.should return expected action');
     expect(it.value).toBe('');
@@ -93,49 +99,22 @@ xdescribe('Action Creator', () => {
 
   it('should correctly set default option on Payload Type dropdown', () => {
     render(<ActionCreator {...props}/>);
+
     expect(screen.getByRole('option', { name: '' }).selected).toBe(true);
     expect(screen.getAllByRole('option')).toHaveLength(6);
   })
 })
 
-/*
-TODO: Implement testing for user events on input values. After spending much time attempting this in the following tests, the result was still failure. Check Action.test.jsx in __tests__ to see a working implementation of second test below. I have tried wrapping the component in context providers, using fireEvent/userEvent. Still no results. If you can get this coverge working, I'm proud of you.
-  - Spearmint V.14
-*/
-const reduxTestCaseState = {
-  id: 1,
-  actionsFile: '',
-  filePath: '',
-  typesFileName: '',
-  typesFilePath: '',
-  type: 'action-creator',
-  actionCreatorFunc: '',
-  actionType: '',
-  payloadKey: null,
-  payloadType: null,
-  it: '',
-}
 
-describe('User input on Action Creator', () => {
-  
-  afterEach(cleanup);
+describe('User Events on Action Creator', () => {
 
   it('updates the It should... input field value on user input', async () => {
     const user = userEvent.setup();
-    // const { getByLabelText } = render(
-    //   <ReduxTestCaseContext.Provider value={[reduxTestCaseState, dispatchtoReduxTestCase]}>
-    //     <ActionCreator/>
-    //   </ReduxTestCaseContext.Provider>
-    //   );
-    const { getByLabelText } = render(<ActionCreator {...props}/>);
+    render(<ActionCreator {...props}/>);
 
-    const it = getByLabelText('It should...');
-    expect(it).toBeInTheDocument();
-    expect(it.placeholder).toBe('e.g.should return expected action');
+    const it = screen.getByRole('textbox', {name: 'It should...'});
     expect(it.value).toBe('');
 
-    // fireEvent.change(it, { target: { value: 'Testing' } });
-    // const newIt = getByLabelText('It should...');
     await user.type(it, 'Testing');
     expect(it.value).toBe('Testing');
   })
@@ -145,11 +124,8 @@ describe('User input on Action Creator', () => {
     render(<ActionCreator {...props}/>);
 
     const actionCreator = screen.getByRole('textbox', {name: 'Action Creator'});
-    expect(actionCreator).toBeInTheDocument();
-    expect(actionCreator.placeholder).toBe('e.g. addTodo');
     expect(actionCreator.value).toBe('');
     
-    // fireEvent.change(actionCreator, { target: { value: 'Testing' } });
     await user.type(actionCreator, 'Testing');
     expect(actionCreator.value).toBe('Testing');
   })
@@ -159,11 +135,8 @@ describe('User input on Action Creator', () => {
     render(<ActionCreator {...props}/>);
 
     const actionType = screen.getByRole('textbox', {name: 'Action Type'});
-    expect(actionType).toBeInTheDocument();
-    expect(actionType.placeholder).toBe('e.g. ADD_TODO');
     expect(actionType.value).toBe('');
     
-    // fireEvent.change(actionType, { target: { value: 'Testing' } });
     await user.type(actionType, 'Testing');
     expect(actionType.value).toBe('Testing');
   })
@@ -173,11 +146,8 @@ describe('User input on Action Creator', () => {
     render(<ActionCreator {...props}/>);
 
     const payloadKey = screen.getByRole('textbox', {name: 'Payload Key'});
-    expect(payloadKey).toBeInTheDocument();
-    expect(payloadKey.placeholder).toBe('e.g. todo');
     expect(payloadKey.value).toBe('');
     
-    // fireEvent.change(payloadKey, { target: { value: 'Testing' } });
     await user.type(payloadKey, 'Testing');
     expect(payloadKey.value).toBe('Testing');
   })
