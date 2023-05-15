3137cb2bd93913314a0fc8e246e561cb7626d05c
Cartlong7 <crlong7@gmail.com>
2023-05-15 10:04:59 -0600

    added props for test

--- src/__tests__/reduxReducerComponent.test.js
+++ src/__tests__/reduxReducerComponent.test.js
@@ -6,3 +6,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect';
+import Reducer from '../components/ReduxTestComponent/Reducer/Reducer';
+
+const props = {
+  reducer: {
+    id: 1,
+    itStatement: '',
+    reducerName: '',
+    initialState: '',
+    reducerAction: '',
+    payloadKey: '',
+    payloadValue: '',
+    expectedKey: '',
+    expectedValue: '',
+  },
+  index: 0
+}
