3a324026d942b0acf112edf7604063645fef8540
Cartlong7 <crlong7@gmail.com>
2023-05-15 10:00:32 -0600

    added test file for reduxReducer component

---
+++ src/__tests__/reduxReducer.test.js
@@ -0,0 +1,8 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom/extend-expect';
